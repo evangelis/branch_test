07bd542b82326ab48ea021a1e28375b8424c8d8b
evangelis <angelisevang@gmail.com>
2020-06-04 17:37:55 +0300

    Second Commit in feature branc

--- Feature.java.java
+++ Feature.java.java
@@ -3,5 +3,6 @@
 public class Feature{
 	public ststic void feature(){
 		System.out.println("1st commit on feature branch");
+		System.out.println("2nd commit on feature branch.\nReady to rebase feature on master branch\n");
 	}
 }
