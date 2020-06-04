50e5c7665910a50dfb22e1f2afc13cc4fa5f5154
evangelis <angelisevang@gmail.com>
2020-06-04 20:16:48 +0300

    Adding a line in feature() that needs also to be pushed to 'origin/feature'

--- Feature.java.java
+++ Feature.java.java
@@ -4,5 +4,6 @@ public class Feature{
 	public ststic void feature(){
 		System.out.println("1st commit on feature branch");
 		System.out.println("2nd commit on feature branch.\nReady to rebase feature on master branch\n");
+		System.out.println("Chnage that needs to be pushed to origin/feature");
 	}
 }
