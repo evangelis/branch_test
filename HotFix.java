8b9f57df20ae5266abeb8fdf8839a212908824b7
evangelis <angelisevang@gmail.com>
2020-06-04 17:02:51 +0300

    Last commnit to hotfix branch before merging into master

--- HotFix.java
+++ HotFix.java
@@ -3,5 +3,7 @@
 public class HotFix{
 	public void fix(){
 		System.out.println("Fixing  bugs in hotfix branch");
+		System.out.println("Releasing last changes in hotfix branch");
+		System.out.println("Ready to merge hotfix branch into master branch") ;
 	}
 }
